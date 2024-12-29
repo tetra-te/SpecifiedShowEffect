9307c996b29ca8dcd72a443e24baa1326980178f
tetra_te <node4853@gmail.com>
2024-12-29 16:21:07 +0900

    Initial commit

---
+++ SpecifiedShowEffect/Effects/SpecifiedShowEffect.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using YukkuriMovieMaker.Commons;
+using YukkuriMovieMaker.Controls;
+using YukkuriMovieMaker.Exo;
+using YukkuriMovieMaker.Player.Video;
+using YukkuriMovieMaker.Plugin.Effects;
+
+namespace SpecifiedShowEffect.Effects
+{
+    [VideoEffect("指定時にのみ表示", ["描画"], [], isAviUtlSupported: false)]
+    internal class SpecifiedShowEffect : VideoEffectBase
+    {
+        public override string Label => "指定時にのみ表示";
+
+        [Display(GroupName = "不透明度", Name = "再生時", Description = "プレビューの再生中の不透明度")]
+        [TextBoxSlider("F1", "%", 0, 100)]
+        [DefaultValue(100d)]
+        [Range(0, 100)]
+        public double Playing { get => playing; set => Set(ref playing, value); }
+        double playing = 100;
+
+        [Display(GroupName = "不透明度", Name = "一時停止時", Description = "プレビューの一時停止中の不透明度")]
+        [TextBoxSlider("F1", "%", 0, 100)]
+        [DefaultValue(100d)]
+        [Range(0, 100)]
+        public double Paused { get => paused; set => Set(ref paused, value); }
+        double paused = 100;
+
+        [Display(GroupName = "不透明度", Name = "出力時", Description = "動画の出力中の不透明度")]
+        [TextBoxSlider("F1", "%", 0, 100)]
+        [DefaultValue(100d)]
+        [Range(0, 100)]
+        public double Exporting { get => exporting; set => Set(ref exporting, value); }
+        double exporting = 100;
+
+        [Display(Name = "最適化", Description = "不透明度が0%の時に拡大率も0%にして高速化します")]
+        [ToggleSlider]
+        public bool OptimizedHiding { get => optimizedHiding; set => Set(ref optimizedHiding, value); }
+        bool optimizedHiding = true;
+
+        public override IEnumerable<string> CreateExoVideoFilters(int keyFrameIndex, ExoOutputDescription exoOutputDescription)
+        {
+            return [];
+        }
+
+        public override IVideoEffectProcessor CreateVideoEffect(IGraphicsDevicesAndContext devices)
+        {
+            return new SpecifiedShowEffectProcessor(this);
+        }
+
+        protected override IEnumerable<IAnimatable> GetAnimatables() => [];
+    }
+}
---
+++ SpecifiedShowEffect/Effects/SpecifiedShowEffectProcessor.cs
@@ -0,0 +1,60 @@
+﻿using System.Numerics;
+using Vortice.Direct2D1;
+using YukkuriMovieMaker.Player.Video;
+
+namespace SpecifiedShowEffect.Effects
+{
+    internal class SpecifiedShowEffectProcessor : IVideoEffectProcessor
+    {
+        readonly SpecifiedShowEffect item;
+        ID2D1Image? input;
+
+        public ID2D1Image Output => input ?? throw new NullReferenceException(nameof(input) + "is null");
+
+        public SpecifiedShowEffectProcessor(SpecifiedShowEffect item)
+        {
+            this.item = item;
+        }
+
+        public DrawDescription Update(EffectDescription effectDescription)
+        {
+            double opacity = 1;
+
+            switch (effectDescription.Usage)
+            {
+                case TimelineSourceUsage.Playing:
+                    opacity = item.Playing / 100;
+                    break;
+                case TimelineSourceUsage.Paused:
+                    opacity = item.Paused / 100;
+                    break;
+                case TimelineSourceUsage.Exporting:
+                    opacity = item.Exporting / 100;
+                    break;
+            }
+
+            var drawDesc = effectDescription.DrawDescription;
+
+            return
+                drawDesc with
+                {
+                    Opacity = drawDesc.Opacity * opacity,
+                    Zoom = (item.OptimizedHiding && opacity == 0) ? new Vector2(0, 0) : drawDesc.Zoom
+                };
+        }
+        public void ClearInput()
+        {
+            input = null;
+        }
+        public void SetInput(ID2D1Image? input)
+        {
+            this.input = input;
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+    }
+}
